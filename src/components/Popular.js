57ba9a198058781259aa810a0e447316def9f85e
oluwadaprof <adeekotobiloba8@gmail.com>
2022-10-06 12:30:06 +0100

    project setup

---
+++ src/components/Popular.js
@@ -0,0 +1,21 @@
+import React, {useState, useEffect} from 'react'
+
+const Popular = () => {
+const [popular, setPopular] = useState([])
+
+    useEffect(() => {
+      getPopular();
+    }, [])
+    
+    const getPopular = async () => {
+        const api = await fetch(`https://api.spoonacular.com/recipes/random?apiKey=${process.env.REACT_APP_API_KEY}&number=9`)
+        const data = await api.json();
+        setPopular(data.recipes)
+        console.log(data)
+    }
+  return (
+    <div>Popular</div>
+  )
+}
+
+export default Popular
