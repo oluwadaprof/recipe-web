bebc11eb0f6ffa0fa8d4e7a45c8c67d589db56b8
franktunde <adeekotobiloba8@gmail.com>
2023-03-16 12:47:16 +0100

    chore: fixed vercel prod error

--- src/pages/Searched.js
+++ src/pages/Searched.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
 import styled from "styled-components";
 import { Link } from "react-router-dom";
+import {motion} from 'framer-motion'
 
 const Searched = () => {
   const [searchedRecipes, setSearchedRecipes] = useState([]);
